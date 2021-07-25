f4118cd7caf381d79e4d3b6d251b0a17301cc9fb
Patrick McLennan <patrick.a.mclennan@gmail.com>
2021-07-25 18:11:57 -0400

    webpack integrated and working in the lambda dir

--- lambda/createAccount.ts
+++ lambda/createAccount.ts
@@ -1,41 +1,45 @@
 import type { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
-import { apiResponse } from '../services/responses.factory';
 import { string, object } from 'joi';
 
 export const createAccountDto = object({
-  firstName: string().max(30),
-  lastName: string().max(30),
-  email: string().email().max(30),
-  picture: string().email().domain(),
-  password: string().min(6).max(30),
+  firstName: string().max(30).required(),
+  lastName: string().max(30).required(),
+  email: string().email().max(30).required(),
+  picture: string().required(),
+  password: string().min(6).max(30).required(),
 });
 
 const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
   if (!event.body)
-    return apiResponse({
+    return {
       statusCode: 400,
-      body: { errors: [`No body was attached to the response.`] },
+      body: JSON.stringify({ errors: [`No body was attached to the response.`] }),
       headers: { 'Content-Type': 'text/plain' },
-    });
+    };
 
-  const body = JSON.parse(event.body);
+  const body = JSON.parse(JSON.stringify(event.body));
   const user = body.user;
   if (!user)
-    return apiResponse({
+    return {
       statusCode: 400,
-      body: { errors: [`Missing Fields.`] },
+      body: JSON.stringify({ errors: [`Missing Fields.`] }),
       headers: { 'Content-Type': 'text/plain' },
-    });
+    };
 
   const { error, value } = createAccountDto.validate(user);
+  if (error) console.error(error);
   if (error)
-    return apiResponse({ statusCode: 400, body: { errors: [error] }, headers: { 'Content-Type': 'text/plain' } });
-  else
-    return apiResponse({
-      statusCode: 200,
-      body: { message: `seems like everything is good` },
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ errors: [error] }),
       headers: { 'Content-Type': 'text/plain' },
-    });
+    };
+  else
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `seems like everything is good` }),
+      headers: { 'Content-Type': 'text/plain' },
+    };
 };
 
 exports.handler = handler;
